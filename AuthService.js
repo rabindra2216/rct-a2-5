9b288cb095cd0d1f770464c208d7284517df0b6c
Rabindra Yadav <rabindrayadav2216@gmail.com>
2023-09-27 18:57:15 +0530

    Intial Cimmit

---
+++ AuthService.js
@@ -0,0 +1,15 @@
+let keyName='user'
+function login(obj){
+    let str=JSON.stringify(obj);
+    localStorage.setItem(keyName,str)
+
+}
+function logout(){
+    localStorage.removeItem(keyName)
+}
+function getUser(){
+    let str=localStorage.getItem(keyName);
+    let obj=str?JSON.parse(str):null;
+    return obj;
+}
+export default{login,logout,getUser}
